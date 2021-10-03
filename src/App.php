793132f59288da32781bdc4b6ebaf268e3e9056f
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2021-10-03 16:10:23 +0700

    WIP handle callback data

--- src/App.php
+++ src/App.php
@@ -3,14 +3,17 @@ namespace App;
 
 use TelegramBot\TelegramBot;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use TelegramBot\Types\CallbackQuery;
 
 class App
 {
     private TelegramBot $bot;
+    private FilesystemAdapter $cache;
 
     function __construct(TelegramBot $bot)
     {
         $this->bot = $bot;
+        $this->cache = new FilesystemAdapter('', 0, dirname(__FILE__) . '../cache/');
     }
 
     function processUpdate()
@@ -18,19 +21,31 @@ class App
         $update = $this->bot->getWebhookUpdate();
 
         if(!empty($update->callback_query)) {
-            $null = '';
+            $keyList = ['quality', 'subs', 'change_quality', 'change_sub', 'cancel', 'ok'];
+            $originalMessage = $update->callback_query->message;
+            $link = $originalMessage->entities[0]->url;
+
+            if($this->checkLink($link)) {
+                $vliveId = explode('/', $link);
+                $vliveId = end($vliveId);
+                $callbackData = $update->callback_query->data;
+
+                foreach($keyList as $key) {
+                    if(strpos($callbackData, $key) !== FALSE) {
+                        $this->handleCallbackQuery($key, $update->callback_query);
+                    }
+                }
+            }
         } else {
             $command = $update->message->getCommand();
             if($command == '/download') {
                 $link = $update->message->getArgs()[0];
-                $re = '/^(http|https):\/\/(.*)vlive\.tv\/post\/(.*)/i';
 
-                if(preg_match($re, $link)) {
+                if($this->checkLink($link)) {
                     $vliveId = explode('/', $link);
                     $vliveId = end($vliveId);
-                    $cache = new FilesystemAdapter();
                     $cacheKey = "vlive_$vliveId";
-                    $item = $cache->getItem($cacheKey);
+                    $item = $this->cache->getItem($cacheKey);
 
                     if($item->isHit()) {
                         $json = $item->get();
@@ -38,13 +53,13 @@ class App
                         exec("youtube-dl -j $link", $output);
                         $json = json_decode($output[0]);
                         $item->set($json);
-                        $cache->save($item);
+                        $this->cache->save($item);
                     }
 
                     $buttons = [0 => []];
                     foreach($json->formats as $format) {
                         if($format->ext == 'mp4') {
-                            $buttons[0][] = $this->bot->buildInlineKeyboardButton($format->height, '', $vliveId . '_format_id');
+                            $buttons[0][] = $this->bot->buildInlineKeyboardButton($format->height, '', "quality_$format->height");
                         }
                     }
 
@@ -65,5 +80,25 @@ class App
                 }
             }
         }
+    }
+
+    private function handleCallbackQuery(String $key, CallbackQuery $cbq)
+    {
+        $keyList = ['quality', 'subs', 'change_quality', 'change_sub', 'cancel', 'ok'];
+        switch ($key) {
+            case 'ok':
+                $msg = $cbq->message->text;
+
+                break;
+            case 'cancel':
+                $this->bot->deleteMessage($cbq->message->chat->id, $cbq->message->message_id);
+                break;
+        }
+    }
+
+    private function checkLink(string $link)
+    {
+        $re = '/^(http|https):\/\/(.*)vlive\.tv\/post\/(.*)/i';
+        return preg_match($re, $link);
     }
 }
